0492abe415a2c4bc7f9c5097e57ebc9c58570a7b
Tyreek Thomas <tyreek1thomas@gmail.com>
2023-03-19 15:27:54 -0500

    Agent profile added

--- Client_Profile.Designer.cs
+++ Client_Profile.Designer.cs
@@ -30,7 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Client_Profile));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btn_logout = new System.Windows.Forms.Button();
+            this.btn_clientlogout = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lbl_clientprofile = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -44,18 +44,18 @@
             this.panel1.Size = new System.Drawing.Size(713, 497);
             this.panel1.TabIndex = 0;
             // 
-            // btn_logout
+            // btn_clientlogout
             // 
-            this.btn_logout.FlatAppearance.BorderSize = 0;
-            this.btn_logout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_logout.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_logout.ForeColor = System.Drawing.Color.White;
-            this.btn_logout.Location = new System.Drawing.Point(12, 472);
-            this.btn_logout.Name = "btn_logout";
-            this.btn_logout.Size = new System.Drawing.Size(123, 37);
-            this.btn_logout.TabIndex = 1;
-            this.btn_logout.Text = "LOG OUT";
-            this.btn_logout.UseVisualStyleBackColor = true;
+            this.btn_clientlogout.FlatAppearance.BorderSize = 0;
+            this.btn_clientlogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_clientlogout.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clientlogout.ForeColor = System.Drawing.Color.White;
+            this.btn_clientlogout.Location = new System.Drawing.Point(12, 472);
+            this.btn_clientlogout.Name = "btn_clientlogout";
+            this.btn_clientlogout.Size = new System.Drawing.Size(123, 37);
+            this.btn_clientlogout.TabIndex = 1;
+            this.btn_clientlogout.Text = "LOG OUT";
+            this.btn_clientlogout.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
@@ -86,7 +86,7 @@
             this.ClientSize = new System.Drawing.Size(892, 521);
             this.Controls.Add(this.lbl_clientprofile);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.btn_logout);
+            this.Controls.Add(this.btn_clientlogout);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Client_Profile";
@@ -100,7 +100,7 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button btn_logout;
+        private System.Windows.Forms.Button btn_clientlogout;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lbl_clientprofile;
     }
